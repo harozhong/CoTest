4fd550b4fa2d908e79da4153acf5b568a14a9bfd
harozhong <harozhong@foxmail.com>
2019-04-19 12:58:13 +0800

    creating initial files

---
+++ test.m
@@ -0,0 +1,9 @@
+% open_system('D:\工作\项目工作\test_model\karlman_lanefit.slx')
+load('D:\工作\吴工\20180929\T025.mat')
+paramNameValStruct.simulationMode = 'normal';
+paramNameValStruct.AbsTol         = '1e-5';
+paramNameValStruct.SaveState      = 'on';
+paramNameValStruct.StateSaveName  = 'xoutnew';
+paramNameValStruct.SaveOutput     ='on';
+paramNameValStruct.OutputSaveName = 'youtnew';
+simout=sim('D:\工作\项目工作\test_model\karlman_lanefit.slx',paramNameValStruct);
