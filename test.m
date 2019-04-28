1171f8cb748f0d2dfed1763aedd22f6578e0ea6f
harozhong <harozhong@foxmail.com>
2019-04-28 15:07:15 +0800

    test12

--- test.m
+++ test.m
@@ -1,10 +1,10 @@
 % open_system('D:\工作\项目工作\test_model\karlman_lanefit.slx')
-load('D:\工作\吴工\20180929\T025.mat')
+load(T025.mat)
 paramNameValStruct.simulationMode = 'normal';
 paramNameValStruct.AbsTol         = '1e-5';
 paramNameValStruct.SaveState      = 'on';
 paramNameValStruct.StateSaveName  = 'xoutnew';
 paramNameValStruct.SaveOutput     ='on';
 paramNameValStruct.OutputSaveName = 'youtnew';
-simout=sim('D:\工作\项目工作\test_model\karlman_lanefit.slx',paramNameValStruct);
-save('D:\工作\项目工作\持续集成\CoTest\simout11.mat', 'simout')
+simout=sim(karlman_lanefit.slx,paramNameValStruct);
+save('D:\Jenkins\workspace\simout11.mat', 'simout')
