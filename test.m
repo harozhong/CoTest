ce572c733bd219a20091bbfff34957d7bd52491f
harozhong <harozhong@foxmail.com>
2019-04-29 10:14:01 +0800

    test23

--- test.m
+++ test.m
@@ -7,4 +7,4 @@ paramNameValStruct.StateSaveName  = 'xoutnew';
 paramNameValStruct.SaveOutput     ='on';
 paramNameValStruct.OutputSaveName = 'youtnew';
 simout=sim('karlman_lanefit.slx',paramNameValStruct);
-save('D:\Jenkins\workspace\simout21.mat', 'simout')
+save('D:\Jenkins\workspace\simout22.mat', 'simout')
